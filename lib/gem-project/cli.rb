c5f0ace995f54f22809a8521dfd16668431d6824
Geoff Elliott <tinywv@gmail.com>
2019-10-31 14:32:14 +0000

    worked out more user input code

--- lib/gem-project/cli.rb
+++ lib/gem-project/cli.rb
@@ -1,16 +1,34 @@
 class CLI
   def run
-    puts " "
-    puts " "
+    puts ""
+    puts ""
     puts ":================================================:"
     puts ":                                                :"
     puts ":  Welcome the English Premier League Info Gem!  :"
     puts ":       Your one stop for all things EPL         :"
     puts ":                                                :"
     puts ":================================================:"
-    puts " "
-    puts " "
+    puts ""
+    puts ""
     
+    Scraper.scrape_teams
+    
+    print_teams
+    puts ""
+    puts ""
+    puts "Enter the number of a team you'd like to learn more about:"
+    
+    input = gets.strip.downcase
+    while input != "exit" do
+      team = Team.all[input.to_i - 1]
+      Scraper.scrap_team_details(team) if !team.stadium
+      print_team(team)
+      puts ""
+      puts "Do you want to learn about another team?"
+      puts "If you do, enter the number of a team to see more info or type 'exit' to get out of the gem."
+      input = gets.strip.downcase
+    end
+    puts "Thanks for stopping by! See ya later!"
   end
     
     
--- lib/gem-project/scraper.rb
+++ lib/gem-project/scraper.rb
@@ -3,15 +3,19 @@ class Scraper
   def self.scrape_teams
     html = open("https://www.premierleague.com/clubs")
     doc = Nokogiri::HTML(html)
-    binding.pry
     
     doc.css("div.nameContainer")[0].each do |team|
       team = Team.new
-      team.name = team.css("h4.clubName").text  
+          binding.pry
+      team.name = team.css("h4.clubName").text 
     end
   end
  
-  
+ def self.scrape_team_details(team)
+   html = open("https://www.premierleague.com/clubs")
+   doc = Nokogiri::HTML(html)
+   team.stadium = doc.css("div.nameContainer")[0].css("div.stadiumName").text
+  end
   
   
 end
