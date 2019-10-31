159ea8e21457ebba5287f77d0b603602b4c6b488
Geoff Elliott <tinywv@gmail.com>
2019-10-31 13:24:10 +0000

    initial CLI welcome set up

--- lib/gem-project/cli.rb
+++ lib/gem-project/cli.rb
@@ -1,5 +1,12 @@
 class CLI
   def run
-   
+    puts " "
+    puts " "
+    puts ":================================================:"
+    puts ":                                                :"
+    puts ":  Welcome the English Premier League Info Gem!  :"
+    puts ":       Your one stop for all things EPL         :"
+    puts ":                                                :"
+    puts ":================================================:"
   end
   
