ae518a1c0bb49ff40d750b0dd5b8c577c5b0518f
Geoff Elliott <tinywv@gmail.com>
2019-10-31 16:51:01 +0000

    stuck and waiting on Nancy, don't wanna lost code

--- lib/gem-project/scraper.rb
+++ lib/gem-project/scraper.rb
@@ -15,8 +15,8 @@ class Scraper
 def self.scrape_team_details(team)
   html = open("https://en.wikipedia.org/wiki/2019%E2%80%9320_Premier_League#Stadiums_and_locations")
   doc = Nokogiri::HTML(html)
-  team.stadium = doc.css("")[0].css("tbody")[0].css("tr")[0].css("td")[2].css("a, title").text
-  end
+  team.stadium = doc.css("table.wikitable.sortable.jquery-tablesorter")[0].css("tbody")[0].css("tr")[0].css("td")[2].css("a, title").text
+end
   
   
 end
